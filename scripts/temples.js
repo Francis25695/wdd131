97eee68394b05a3a0dc68c1b0916d9160615394b
Francis25695 <francismwaka99@gmail.com>
2026-07-13 22:15:14 +0300

    image album

---
+++ scripts/temples.js
@@ -0,0 +1,14 @@
+const temples = [
+    {
+        templeName: "Aba Nigeria Temple",
+        location: "Aba, Nigeria",
+        imageUrl: "images/aba-nigeria.jpg",
+        dedicated: "2005"
+    },
+    {
+        templeName: "Accra Ghana Temple",
+        location: "Accra, Ghana",
+        imageUrl: "images/accra-ghana.jpg",
+        dedicated: "2004"
+    }
+];
